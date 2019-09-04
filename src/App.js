7e1b2d5b85e52abf3114cdc82d8850609c8534ba
admin <you@example.com>
2019-09-04 22:38:35 +0800

    注册

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from 'react'
 import axios from 'axios'
+import ReactDOM from 'react-dom';
+// import { Button } from 'react-weui';
+//import styles
+import 'weui';
+import 'react-weui/build/packages/react-weui.css';
+import { ButtonArea,
+  Button,
+  CellsTitle,
+  CellsTips,
+  Cell,
+  CellHeader,
+  CellBody,
+  CellFooter,
+  Form,
+  FormCell,
+  Icon,
+  Input,
+  Label,
+  TextArea,
+  Switch,
+  Radio,
+  Checkbox,
+  Select,
+  VCode,
+  Agreement,
+  Toptips
+} from 'react-weui';
+
+
 class App extends React.Component {
   state = {
     c: 'c',
@@ -47,9 +76,46 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>test</h1>
-        <h1>{this.state.c}</h1>
-        <h1>{this.state.s}</h1>
+        <CellsTitle>实名注册</CellsTitle>
+            <Form>
+                <FormCell>
+                    <CellHeader>
+                        <Label>姓名</Label>
+                    </CellHeader>
+                    <CellBody>
+                        <Input type="txt" placeholder="输入姓名"/>
+                    </CellBody>
+                </FormCell>
+                <FormCell vcode>
+                    <CellHeader>
+                        <Label>手机号</Label>
+                    </CellHeader>
+                    <CellBody>
+                        <Input type="tel" placeholder="输入手机号"/>
+                    </CellBody>
+                    <CellFooter>
+                        <Button type="vcode">发送</Button>
+                    </CellFooter>
+                </FormCell>
+                <FormCell vcode>
+                    <CellHeader>
+                        <Label>验证码</Label>
+                    </CellHeader>
+                    <CellBody>
+                        <Input type="number" placeholder="验证码"/>
+                    </CellBody>
+                </FormCell>
+                <FormCell>
+                    <CellHeader>
+                        <Label>身份证</Label>
+                    </CellHeader>
+                    <CellBody>
+                        <Input type="txt" placeholder="输入身份证"/>
+                    </CellBody>
+                </FormCell>
+            </Form>
+            <CellsTips>东至政务中心</CellsTips>
+            <Button>注册</Button>
       </div>
     )
   }
